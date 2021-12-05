dd7847a3bdf28583642181ab83c320479950e491
TimRaben <timraben07@gmail.com>
2021-12-05 14:39:10 +0100

    init1
    init1

--- commands/earn.js
+++ commands/earn.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send(`<@&292953664492929025> bal ${message.author}`)
+    return message.channel.send(`<@292953664492929025> bal ${message.author}`)
 }
 
 module.exports.help = {
