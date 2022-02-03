68958d0b16e57faebccca21e5df7d89200dc2fce
TimRaben <timraben07@gmail.com>
2022-02-03 15:27:43 +0100

    init1
    init1

--- commands/picklock.js
+++ commands/picklock.js
@@ -6,6 +6,13 @@ module.exports.run = async (client, message, args) => {
 
     var kanaal = message.member.guild.channels.cache.get("938757493516042290");
 
+    let responses = [
+        "**Gelukt**, je kan nu naar binnen gaan en je inbraak doorzetten.",
+        "**Niet Gelukt**, je bent gefaald, probeer het een andere keer opnieuw, ga snel weg voordat de Politie er is."
+    ]
+
+    let Response = responses[Math.floor(Math.random()*(responses.length)-1)]
+
     const embed2 = new discord.MessageEmbed()
         .setTitle("Spijkenisse Roleplay - Inbraak")
         .setDescription(`**MELDING** Er word momenteel ingebroken met een lockpicktel, de melding is gedaan door een buurt bewoner, de verdachte is mogelijk ${args[0]}!`)
@@ -13,17 +20,26 @@ module.exports.run = async (client, message, args) => {
         .setFooter("Spijkenisse Roleplay - Inbraak");
 
     const embed3 = new discord.MessageEmbed()
-    .setTitle("Spijkenisse Roleplay - Inbraak")
-    .setDescription(`Gelukt!`)
-    .setAuthor("Inbraak - Spijkenisse Roleplay")
-    .setFooter("Spijkenisse Roleplay - Inbraak");
+        .setTitle("Spijkenisse Roleplay - Inbraak")
+        .setDescription(`Je hebt een Inbraak gestart, je gebruikt nu je lockpicktels om de deur open te maken!\n\nTijd Resterend: 30 Seconde.`)
+        .setAuthor("Inbraak - Spijkenisse Roleplay")
+        .setFooter("Spijkenisse Roleplay - Inbraak");
+
+    const embed4 = new discord.MessageEmbed()
+        .setTitle("Spijkenisse Roleplay - Inbraak")
+        .setDescription(`De Inbraak is ${Response}`)
 
 
         kanaal.send(embed2, "<@&866336898400911361>");
 
+    message.channel.send(embed3)
 
+    setTimeout(() => {
 
-        message.channel.send(embed3)
+        message.channel.send(embed4)
+
+    }, 30000);
+
 
 
 }
