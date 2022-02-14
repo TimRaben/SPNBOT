b2643942ae58d679a691963d0d8831ea80c35391
TimRaben <timraben07@gmail.com>
2022-02-14 13:29:35 +0100

    init1
    init1

---
+++ commands/msgembed.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("⛔ **|** Sorry! Jij hebt niet de juiste bevoegdheid om iemand te whitelisten.");
+
+    if (!args[0]) return message.reply("⛔ **|** Geef geen geldige persoon op.");
+
+    var user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    var embedParent = new discord.MessageEmbed()
+        .setTitle(`Spijkenisse Roleplay - Netwerk`)
+        .setDescription(`${args.slice(1).join(" ")}`)
+        .setAuthor("https://media.discordapp.net/attachments/838112437239939133/918131398991315074/unknown.png?width=679&height=678", "Spijkenisse Netwerk")
+        .setThumbnail("https://media.discordapp.net/attachments/838112437239939133/918131398991315074/unknown.png?width=679&height=678")
+        .setFooter('Spijkenisse Roleplay - Netwerk')
+
+    user.send(`${args.slice(1).join(" ")}`)
+
+    message.channel.send(`Je hebt succesvol een Embed Prive bericht gestuurd naar ${args[0]}\n\nInhoud Bericht:\n${args.slice(1).join(" ")}`)
+
+}
+
+module.exports.help = {
+    name: "msg" 
+}
