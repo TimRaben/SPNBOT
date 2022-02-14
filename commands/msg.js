d95dc183e8ad3780322703a40356ac768d5d13e1
TimRaben <timraben07@gmail.com>
2022-02-14 13:19:13 +0100

    init1
    init1

--- commands/msg.js
+++ commands/msg.js
@@ -2,6 +2,10 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("⛔ **|** Sorry! Jij hebt niet de juiste bevoegdheid om iemand te whitelisten.");
+
+    if (!args[0]) return message.reply("⛔ **|** Geef geen geldige persoon op.");
+
     var user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
     user.send(`${args.slice(1).join(" ")}`)
